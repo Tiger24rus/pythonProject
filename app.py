dc8af3eae74d56f3f12d7a30abc11928c5429878
Konstantin_Shishin <shishinkostya2000@gmail.com>
2023-11-30 00:19:07 +0700

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import * # Импорт всего пакета flask
+app = Flask(__name__)  # Создание экземпляра приложения Flask
+
+# Определение маршрута для главной страницы
+@app.route('/')
+def index():
+    return render_template('index.html')  # Возврат шаблона index.html
+
+# Определение маршрута для страницы контактов
+@app.route('/contacts/')
+def contacts():
+    return render_template('contacts.html')  # Возврат шаблона contacts.html
+
+# Проверка, является ли файл, в котором выполняется данный код, главным
+if __name__ == "main":
+     app.run()  # Запуск приложения
